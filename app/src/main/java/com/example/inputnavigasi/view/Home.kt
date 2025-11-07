e4bf1a3c1c7de95fadaaca8a5942c73413f19304
naufalmuhdaffa <naufaldaffa878@gmail.com>
2025-11-07 15:15:55 +0700

    Menambahkan resource dimen di dimens.xml dan menggunakan resource dimen di Home.kt

--- app/src/main/java/com/example/inputnavigasi/view/Home.kt
+++ app/src/main/java/com/example/inputnavigasi/view/Home.kt
@@ -6,10 +6,16 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.inputnavigasi.R
 
 @Composable
 fun HomeScreen(
@@ -20,7 +26,7 @@ fun HomeScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(horizontal = 24.dp),
+                .padding(horizontal = dimensionResource(R.dimen.padding_medium)),
             horizontalAlignment = Alignment.CenterHorizontally
         ){
             Spacer(Modifier.height(32.dp))
