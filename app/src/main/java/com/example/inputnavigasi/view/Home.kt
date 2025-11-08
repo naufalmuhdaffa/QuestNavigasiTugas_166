dbef6e3018a8294b2ab4ba92a996967fc9e79cfe
naufalmuhdaffa <naufaldaffa878@gmail.com>
2025-11-08 09:10:34 +0700

    Menambahkan file, komponen, dan layout

--- app/src/main/java/com/example/inputnavigasi/view/Home.kt
+++ app/src/main/java/com/example/inputnavigasi/view/Home.kt
@@ -1,17 +1,20 @@
 package com.example.inputnavigasi.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -39,6 +42,16 @@ fun HomeScreen(
             )
 
             Spacer(Modifier.weight(1f))
+
+            Column(horizontalAlignment = Alignment.CenterHorizontally){
+                Image(
+                    painter = painterResource(id = R.drawable.image),
+                    contentDescription = "Logo Aplikasi",
+                    modifier = Modifier
+                        .size(96.dp)
+                        .padding(bottom = 8.dp)
+                )
+            }
         }
     }
 }
